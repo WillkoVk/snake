c8c1991452619f3eb0906778a8997ff2dd1195f9
WillkoVk <kurchinaskayavera@gmail.com>
2016-03-23 12:08:59 +0300

    Добавлены линии, змейка

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,31 +10,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            // отрисовка рамки
+            HorizontLine upLine = new HorizontLine(0, 78, 0, '+');
+            HorizontLine downLine = new HorizontLine(0, 78, 24, '+');
+            VertLine leftLine = new VertLine(0, 24, 0, '+');
+            VertLine rightLine = new VertLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            // отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+            snake.Move();
 
             Console.ReadLine();
         }
